0ead42cd8b16c75895a6a3b7f1a6bd5bdaec5c87
Anibal Fuentes Jara <anibalfuentesjara@gmail.com>
2020-03-09 21:48:48 -0300

    add notes

---
+++ src/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+  return (<p className="Main"> Que tal </p>);
+}
+
+export default Main;
---
+++ src/Sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (<aside className="Sidebar"> Hola mundo </aside>);
+}
+
+export default Sidebar;
